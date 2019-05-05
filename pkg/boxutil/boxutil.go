1b24bee4e376f46445f74af9e6c8cda66f980d3a
stutonk <xealblade@gmail.com>
2019-05-05 14:24:47 -0400

    Generalize pipeline and add error propegation
    The middle stage of the pipeline has been changed to a single
    function which accepts a transformation function. This allows the
    specific code to be located in the parent function where it is intended
    to be used.
    
    Error channels were added to pipeline stages in order to communicate
    them to the main goroutine.
    
    An error was corrected in the gen stage that would send a partially full
    as well as an empty chunk down the pipeline.
    
    A new error type was added to indicate the condition when decryption is
    attempted with the incorrect key.
    
    Documentation has been added for all functions and improved overall

--- pkg/boxutil/boxutil.go
+++ pkg/boxutil/boxutil.go
@@ -7,6 +7,7 @@ package boxutil
 
 import (
 	"crypto/rand"
+	"errors"
 	"io"
 	"runtime"
 
@@ -19,42 +20,40 @@ const (
 	keyLen    = 32
 	memory    = 64 * 1024
 	nonceLen  = 24
-	time      = 1
+	passes    = 1
 )
 
-func gen(in io.Reader, size int) <-chan *[]byte {
+// ErrIncorrectKey should be used when an Open operation is attempted with
+// the wrong key
+var ErrIncorrectKey = errors.New("incorrect key")
+
+type functor func(*[]byte) (*[]byte, error)
+
+// gen is the first stage of a concurrent pipeline. It reads
+// chunkSize-sized byte slices from the Reader (or however much is left)
+// and sends them down the pipeline
+func gen(in io.Reader, size int, e chan<- error) <-chan *[]byte {
+	if size < 1 {
+		return nil
+	}
 	out := make(chan *[]byte)
 	go func() {
-		for more := true; more; /**/ {
+		defer close(out)
+		for {
 			chunk := make([]byte, size)
-			if n, err := in.Read(chunk); err != nil {
+			n, err := in.Read(chunk)
+			if err != nil {
 				if err == io.EOF {
-					chunk = chunk[:n]
-					more = false
+					break
 				} else {
-					panic(err)
+					e <- err
+					return
 				}
+			} else if n < size {
+				chunk = chunk[:n]
 			}
 			out <- &chunk
 		}
-		close(out)
-	}()
-	return out
-}
-
-func open(in <-chan *[]byte, key *[keyLen]byte) <-chan *[]byte {
-	out := make(chan *[]byte)
-	go func() {
-		for data := range in {
-			var nonce [nonceLen]byte
-			copy(nonce[:], (*data)[:nonceLen])
-			opened, ok := secretbox.Open(nil, (*data)[nonceLen:], &nonce, key)
-			if !ok {
-				panic("incorrect key")
-			}
-			out <- &opened
-		}
-		close(out)
 	}()
 	return out
 }
@@ -62,12 +61,24 @@ func open(in <-chan *[]byte, key *[keyLen]byte) <-chan *[]byte {
 /*
 OpenStream applies secretbox.Open to an io.Reader stream of []byte chunks
 and writes them to an io.Writer stream. Each chunk should begin with the
-nonce used to seal the chunk.
+nonce used to seal the chunk. If this function returns an error, the data
+written to the output stream should be considered corrupted and discarded.
 */
-func OpenStream(r io.Reader, w io.Writer, key *[keyLen]byte) {
-	in := gen(r, nonceLen+chunkSize+secretbox.Overhead)
-	out := open(in, key)
-	write(out, w)
+func OpenStream(r io.Reader, w io.Writer, key *[keyLen]byte) error {
+	e := make(chan error)
+	defer close(e)
+	in := gen(r, nonceLen+chunkSize+secretbox.Overhead, e)
+	f := func(data *[]byte) (*[]byte, error) {
+		var nonce [nonceLen]byte
+		copy(nonce[:], (*data)[:nonceLen])
+		opened, ok := secretbox.Open(nil, (*data)[nonceLen:], &nonce, key)
+		if !ok {
+			return nil, ErrIncorrectKey
+		}
+		return &opened, nil
+	}
+	out := work(in, f, e)
+	return write(out, w, e)
 }
 
 /*
@@ -79,45 +90,75 @@ documentation.
 func Passkey(password, salt []byte) *[keyLen]byte {
 	var key [keyLen]byte
 	cpus := uint8(runtime.NumCPU())
-	keyBytes := argon2.IDKey(password, salt, time, memory, cpus, keyLen)
+	keyBytes := argon2.IDKey(password, salt, passes, memory, cpus, keyLen)
 	copy(key[:], keyBytes)
 	return &key
-}
-
-func seal(in <-chan *[]byte, key *[keyLen]byte) <-chan *[]byte {
-	out := make(chan *[]byte)
-	go func() {
-		for data := range in {
-			nonceBytes := make([]byte, nonceLen)
-			if _, err := rand.Read(nonceBytes); err != nil {
-				panic(err)
-			}
-			var nonce [nonceLen]byte
-			copy(nonce[:], nonceBytes)
-			sealed := secretbox.Seal(nonceBytes, *data, &nonce, key)
-			out <- &sealed
-		}
-		close(out)
-	}()
-	return out
 }
 
 /*
 SealStream applies secretbox.Seal to an io.Reader stream of []byte chunks
 and writes them to an io.Writer stream. Each resulting chunk will be
 prepended with the nonce used in its creation and will additionally be
-secretbox.Overhead bytes longer.
+secretbox.Overhead bytes longer. If this function returns a non-nil error,
+the data written to the output stream should be considered corrupted and
+discarded.
 */
-func SealStream(r io.Reader, w io.Writer, key *[keyLen]byte) {
-	in := gen(r, chunkSize)
-	out := seal(in, key)
-	write(out, w)
+func SealStream(r io.Reader, w io.Writer, key *[keyLen]byte) error {
+	e := make(chan error)
+	defer close(e)
+	in := gen(r, chunkSize, e)
+	f := func(data *[]byte) (*[]byte, error) {
+		nonceBytes := make([]byte, nonceLen)
+		if _, err := rand.Read(nonceBytes); err != nil {
+			// this may happen if the system runs out of entropy
+			// it may be better to wait for more instead
+			return nil, err
+		}
+		var nonce [nonceLen]byte
+		copy(nonce[:], nonceBytes)
+		sealed := secretbox.Seal(nonceBytes, *data, &nonce, key)
+		return &sealed, nil
+	}
+	out := work(in, f, e)
+	return write(out, w, e)
 }
 
-func write(in <-chan *[]byte, w io.Writer) {
-	for data := range in {
-		if _, err := w.Write(*data); err != nil {
-			panic(err)
+// work is the middle stage of a concurrent pipeline. It applies a
+// transformation function f to incoming data and passes it down the pile.
+// Errors upstream should close their send channel so this goroutine will
+// terminate.
+func work(in <-chan *[]byte, f functor, e chan<- error) <-chan *[]byte {
+
+	out := make(chan *[]byte)
+	go func() {
+		defer close(out)
+		for data := range in {
+			v, err := f(data)
+			if err != nil {
+				e <- err
+				return
+			}
+			out <- v
+		}
+	}()
+	return out
+}
+
+// write is the final stage of a concurrent pipeline. It commits transformed
+// data to an output stream. Errors upstream should close their send channel
+// so this function can abort with an error.
+func write(in <-chan *[]byte, w io.Writer, e <-chan error) error {
+	for {
+		select {
+		case data, ok := <-in:
+			if !ok {
+				return nil
+			}
+			if _, err := w.Write(*data); err != nil {
+				return err
+			}
+		case err := <-e:
+			return err
 		}
 	}
 }
